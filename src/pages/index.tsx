d5f0af2ea6e12354c305abdfe0da266daa01a0b5
Rajarshi Nigam <raj@onepeloton.com>
2020-04-29 12:37:46 -0400

    use something that needs a polyfill

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import Foo from "@peloton/foo";
 
-const Home = () => (
-  <div>
-    <Foo />
-  </div>
-);
+const Home = () => {
+  console.log(
+    Object.fromEntries([
+      ["foo", 2],
+      ["bar", 3],
+    ])
+  );
+
+  return (
+    <div>
+      <Foo />
+    </div>
+  );
+};
 
 export default Home;
